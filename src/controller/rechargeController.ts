89d4c6d0061bcf250e1db95fb3149d1ed0ab1458
Sulekh <sulekhs@cybage.com>
2022-06-30 21:10:35 +0530

    updated with axios

--- src/controller/rechargeController.ts
+++ src/controller/rechargeController.ts
@@ -3,6 +3,7 @@ import logger from "../utils/logger";
 import { CreateRechargeInput, updateRechargeInput } from "../validations/rechargeValidation";
 import { RechargeService } from "../service/rechargeService";
 import fetch from "node-fetch";
+import axios from "axios";
 //import { ParsedQs } from "qs";
 
 interface UpdateObj{
@@ -49,6 +50,41 @@ export class RechargeController {
     };
 
 
+    // //Api For Updating The Status of Recahrge to 1 that is Success
+    // changeStatusHandler:any = async (req: Request<updateRechargeInput['params'], {}, CreateRechargeInput["body"]>, res: Response) => {
+    //     try {
+    //         const updateObj:UpdateObj={};
+    //         updateObj.status = 1;
+    //         const rechargeId = req.params.rechargeId;
+    //         const rechargeTable = await this.RechargeService.findRecharge({rechargeId});
+    //         const entryId = rechargeTable?.yourId;
+    //         if (!rechargeTable) {
+    //             console.log("rechargeId Not Found!!!");
+    //             return res.sendStatus(404);
+    //         }
+    //         else{
+    //             const recharge = await this.RechargeService.findAndUpdateRecharge({
+    //                 rechargeId}, updateObj, {new: true} 
+    //             ).then(async updated => {
+    //                 const rechargeTable = await this.RechargeService.findRecharge({rechargeId})
+    //                 .then(async found => {
+    //                     await fetch("http://localhost:4500/api/callback/" + entryId, {
+    //                     method: "PUT",
+    //                     headers: {"Content-Type": "application/json"},
+    //                     body: JSON.stringify({ "status":1 })
+    //                     })
+    //                         return res.status(201).json(updated);
+    //                 })
+    //             })
+                
+    //             return res.status(201).json(recharge);
+    //         }
+            
+    //     } catch (e:any) {
+    //         logger.error(e);
+    //         return res.status(500).send(e.message);
+    //     }
+    // }
     //Api For Updating The Status of Recahrge to 1 that is Success
     changeStatusHandler:any = async (req: Request<updateRechargeInput['params'], {}, CreateRechargeInput["body"]>, res: Response) => {
         try {
@@ -57,27 +93,40 @@ export class RechargeController {
             const rechargeId = req.params.rechargeId;
             const rechargeTable = await this.RechargeService.findRecharge({rechargeId});
             const entryId = rechargeTable?.yourId;
-            if (!rechargeTable) {
-                console.log("rechargeId Not Found!!!");
-                return res.sendStatus(404);
-            }
-            else{
-                const recharge = await this.RechargeService.findAndUpdateRecharge({
-                    rechargeId}, updateObj, {new: true} 
-                ).then(async updated => {
-                    const rechargeTable = await this.RechargeService.findRecharge({rechargeId})
-                    .then(async found => {
-                        await fetch("http://localhost:4500/api/callback/" + entryId, {
-                        method: "PUT",
-                        headers: {"Content-Type": "application/json"},
-                        body: JSON.stringify({ "status":1 })
-                        })
-                            return res.status(201).json(updated);
-                    })
-                })
+            
+            const recharge = await this.RechargeService.findAndUpdateRecharge({
+                rechargeId}, updateObj, {new: true} 
+            );
+
+            const userStatus = JSON.stringify({
+                "status" : updateObj.status,
+            });
+            //CallBack to User Side Api 
+            const callbackUpdateRechargeRequest = axios({
+                baseURL: "http://localhost:4500/api/callback/" + entryId,
+                headers: {"Content-Type": "application/json"},
+                method: "PUT",
+                data: userStatus
+            });
+
+            return res.status(201).json(callbackUpdateRechargeRequest);
+            //res.status(201).json(entryRequest);
+                // const recharge = await this.RechargeService.findAndUpdateRecharge({
+                //     rechargeId}, updateObj, {new: true} 
+                // ).then(async updated => {
+                //     const rechargeTable = await this.RechargeService.findRecharge({rechargeId})
+                //     .then(async found => {
+                //         await fetch("http://localhost:4500/api/callback/" + entryId, {
+                //         method: "PUT",
+                //         headers: {"Content-Type": "application/json"},
+                //         body: JSON.stringify({ "status":1 })
+                //         })
+                //             return res.status(201).json(updated);
+                //     })
+                // })
                 
-                return res.status(201).json(recharge);
-            }
+                
+            
             
         } catch (e:any) {
             logger.error(e);
